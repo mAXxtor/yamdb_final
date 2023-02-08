29318e44387894237e056aa51978f1c7db088769
Max Verbitskiy <maxxtor@yandex.ru>
2023-02-08 22:57:45 +0400

    fix test db

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-if os.getenv('TEST_DB'):
+if os.getenv('TEST_DB', default=False):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
